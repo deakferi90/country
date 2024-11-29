ce76f85f66ce0085885db1b004907b3b46933bcf
Ferenc Deak <ferenc.deak@accesa.eu>
2024-11-29 12:43:33 +0200

    implemented toggle theme for header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,27 +12,22 @@ import { SwitchComponent } from './switch/switch.component';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  dark: boolean = false; // Boolean flag to represent the current theme (light or dark)
+  dark: boolean = false;
 
   constructor(
     private themeService: ThemeService,
-    @Inject(PLATFORM_ID) private platformId: Object // Inject PLATFORM_ID
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {}
 
   ngOnInit(): void {
-    // Check if the code is running in the browser
     if (isPlatformBrowser(this.platformId)) {
-      // Initialize the current theme from the service
-      this.dark = this.themeService.getCurrentTheme() === 'dark'; // Set dark or light based on service
-      // Apply the theme to the document body
+      this.dark = this.themeService.getCurrentTheme() === 'dark';
       document.body.className = this.dark ? 'dark' : 'light';
     }
   }
 
-  // Method to update the theme based on the event from the child component
   onThemeChange(isDark: boolean): void {
     this.dark = isDark;
-    // Save the new theme to localStorage and update the document body
     this.themeService.setThemeToLocalStorage(isDark ? 'dark' : 'light');
 
     if (isPlatformBrowser(this.platformId)) {
